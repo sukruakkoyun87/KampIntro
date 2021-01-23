21f5231e1dbdef1aecfed1b5bcca729a520d7a6e
şükrü akkoyun <Sukruakkoyun87@hotmail.com>
2021-01-24 00:48:01 +0300

    Düzenleme 2

--- ClassIntro/Program.cs
+++ ClassIntro/Program.cs
@@ -48,7 +48,6 @@ namespace ClassIntro
 
     class Kurs
     {
-
         public string KursAdi { get; set; }
         public string Egitmen { get; set; }
         public int IzlenmeOrani { get; set; }
